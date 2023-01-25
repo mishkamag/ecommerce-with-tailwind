18aed3b95ae075168b8c98f81598424021ce9118
mishkamag <m93.maglaperidze@gmail.com>
2023-01-25 14:44:55 +0400

    style changes

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -6,27 +6,27 @@ import electronics from "../assets/images/electronics.png";
 
 const Categories = () => {
   return (
-    <div className="w-11/12 mt-12">
-      <div className="flex justify-around	">
-        <div className="space-y-2 text-center">
+    <div className="w-11/12 mt-20 ">
+      <div className="flex justify-around items-center	">
+        <div className="space-y-2 text-center cursor-pointer">
           <div className="w-24 h-16 flex items-center justify-center rounded-md">
-            <img src={mobile} alt="mobile" />
+            <img className="w-12 h-auto" src={mobile} alt="mobile" />
           </div>
           <h1 className="font-semibold">Mobiles</h1>
         </div>
-        <div className="space-y-2 text-center">
+        <div className="space-y-2 text-center cursor-pointer">
           <div className="w-24 h-16 flex items-center justify-center rounded-md">
             <img src={watches} alt="watches" />
           </div>
           <h1 className="font-semibold">Watches</h1>
         </div>
-        <div className="space-y-2 text-center">
+        <div className="space-y-2 text-center cursor-pointer">
           <div className="w-24 h-16 flex items-center justify-center rounded-md">
             <img src={furniture} alt="Furniture" />
           </div>
           <h1 className="font-semibold">Furnitures</h1>
         </div>
-        <div className="space-y-2 text-center">
+        <div className="space-y-2 text-center cursor-pointer">
           <div className="w-24 h-16 flex items-center justify-center rounded-md">
             <img src={electronics} alt="electornics" />
           </div>
