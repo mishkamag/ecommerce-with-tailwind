0830b19be27fd53853c2075ddb264477672e8f3f
mishkamag <m93.maglaperidze@gmail.com>
2023-01-31 22:16:16 +0400

    splited home component

---
+++ src/components/HomeSections.js
@@ -0,0 +1,44 @@
+import { IoMdArrowForward } from "react-icons/io";
+import useWindowResize from "../hooks/useWindowResize";
+import Product from "./Product";
+
+const HomeSections = (props) => {
+  const { title, categoryProducts, id } = props;
+
+  const size = useWindowResize();
+  let cardsToShow = 1;
+
+  if (size.width >= 576) {
+    cardsToShow = 1;
+  }
+  if (size.width >= 768) {
+    cardsToShow = 2;
+  }
+  if (size.width >= 992) {
+    cardsToShow = 3;
+  }
+  if (size.width >= 1200) {
+    cardsToShow = 5;
+  }
+  return (
+    <div>
+      <section id={id} className="py-16">
+        <div className="container mx-auto">
+          <div className="flex justify-between items-center">
+            <h1 className="text-[#008ECC] mb-4 pb-2  uppercase text-2xl font-bold border-b border-black">
+              {title}
+            </h1>
+            <IoMdArrowForward className="text-2xl text-[#008ECC]" />
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px]  ">
+            {categoryProducts.slice(0, cardsToShow).map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HomeSections;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,11 @@
 import React, { useContext } from "react";
-import { IoMdArrowForward } from "react-icons/io";
-import useWindowResize from "../hooks/useWindowResize";
 import { ProductContext } from "../store/ProductContext";
-import Product from "../components/Product";
 import Categories from "../components/Categories";
 import Hero from "../components/Hero";
+import HomeSections from "../components/HomeSections";
 
 const Home = () => {
-  const { products, categoryFilter } = useContext(ProductContext);
-
-  const size = useWindowResize();
-  let cardsToShow = 1;
-
-  if (size.width >= 576) {
-    cardsToShow = 1;
-  }
-  if (size.width >= 768) {
-    cardsToShow = 2;
-  }
-  if (size.width >= 992) {
-    cardsToShow = 3;
-  }
-  if (size.width >= 1200) {
-    cardsToShow = 5;
-  }
+  const { categoryFilter } = useContext(ProductContext);
 
   const menProducts = categoryFilter("men's clothing");
   const womenProducts = categoryFilter("women's clothing");
@@ -36,69 +18,26 @@ const Home = () => {
 
       <Hero />
 
-      <section id="men" className="py-16">
-        <div className="container mx-auto">
-          <div className="flex justify-between items-center">
-            <h1 className="text-[#008ECC] mb-4 pb-2  uppercase text-2xl font-bold border-b border-black">
-              Men category
-            </h1>
-            <IoMdArrowForward className="text-2xl text-[#008ECC]" />
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px]  ">
-            {menProducts.slice(0, cardsToShow).map((product) => {
-              return <Product product={product} key={product.id} />;
-            })}
-          </div>
-        </div>
-      </section>
-
-      <section id="women" className="py-16">
-        <div className="container mx-auto">
-          <div className="flex justify-between items-center">
-            <h1 className="text-[#008ECC] mb-4 pb-2  uppercase text-2xl font-bold border-b border-black  ">
-              Women category
-            </h1>
-            <IoMdArrowForward className="text-2xl text-[#008ECC]" />
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px]  ">
-            {womenProducts.slice(0, cardsToShow).map((product) => {
-              return <Product product={product} key={product.id} />;
-            })}
-          </div>
-        </div>
-      </section>
-
-      <section id="jewelery" className="py-16">
-        <div className="container mx-auto">
-          <div className="flex justify-between items-center">
-            <h1 className="text-[#008ECC] mb-4 pb-2  uppercase text-2xl font-bold border-b border-black  ">
-              Jewelery category
-            </h1>
-            <IoMdArrowForward className="text-2xl text-[#008ECC]" />
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px]  ">
-            {jeweleryProducts.slice(0, cardsToShow).map((product) => {
-              return <Product product={product} key={product.id} />;
-            })}
-          </div>
-        </div>
-      </section>
-
-      <section id="electronics" className="py-16">
-        <div className="container mx-auto">
-          <div className="flex justify-between items-center">
-            <h1 className="text-[#008ECC] mb-4 pb-2  uppercase text-2xl font-bold border-b border-black  ">
-              Electronics category
-            </h1>
-            <IoMdArrowForward className="text-2xl text-[#008ECC]" />
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px]  ">
-            {electronicsProducts.slice(0, cardsToShow).map((product) => {
-              return <Product product={product} key={product.id} />;
-            })}
-          </div>
-        </div>
-      </section>
+      <HomeSections
+        title={"Men category"}
+        categoryProducts={menProducts}
+        id={"men"}
+      />
+      <HomeSections
+        title={"Women category"}
+        categoryProducts={womenProducts}
+        id={"women"}
+      />
+      <HomeSections
+        title={"Jewelery category"}
+        categoryProducts={jeweleryProducts}
+        id={"jewelery"}
+      />
+      <HomeSections
+        title={"Electronics category"}
+        categoryProducts={electronicsProducts}
+        id={"electronics"}
+      />
     </div>
   );
 };
