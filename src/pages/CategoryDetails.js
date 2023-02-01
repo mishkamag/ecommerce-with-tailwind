706b394abf3721a931ff570ab646eae27e7ce5d9
mishkamag <m93.maglaperidze@gmail.com>
2023-02-01 20:32:53 +0400

    fineshed category details component

--- src/pages/CategoryDetails.js
+++ src/pages/CategoryDetails.js
@@ -7,19 +7,15 @@ const CategoryDetails = () => {
   const { products } = useContext(ProductContext);
   const { category } = useParams();
 
-  const product = products.find((item) => {
-    return item.category === category;
-  });
-
-  // console.log(product);
-  // console.log(products.category);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px]  ">
-      {products
-        .filter((product) => product.category)
-        .map((product) => {
-          return <Product product={product} key={product.id} />;
-        })}
+    <div className="container mx-auto my-24">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px]  ">
+        {products
+          .filter((product) => product.category === category)
+          .map((product) => {
+            return <Product product={product} key={product.id} />;
+          })}
+      </div>
     </div>
   );
 };
