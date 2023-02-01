98af3aee49d321b02ed7837b85f2ba1460b2bf56
mishkamag <m93.maglaperidze@gmail.com>
2023-02-01 20:15:19 +0400

    fixed slider bug, and commited map coz it has bug

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import img1 from "../assets/sliderImg/img1.png";
 import img2 from "../assets/sliderImg/img2.png";
 import img3 from "../assets/sliderImg/img3.png";
@@ -7,74 +7,62 @@ import img5 from "../assets/sliderImg/img5.png";
 import { FaArrowCircleLeft } from "react-icons/fa";
 import { BsArrowRightCircleFill } from "react-icons/bs";
 
-const featuredProducts = [img1, img2, img3, img4, img5];
-
+const sliderImages = [img1, img2, img3, img4, img5];
 let count = 0;
-let slideInterval;
+
 const Hero = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [imageIndex, setImageIndex] = useState(0);
+  const sliderRef = useRef();
 
-  const slideRef = useRef();
-
-  const removeAnimation = () => {
-    slideRef.current.classList.remove("fade-anim");
+  const startSlider = () => {
+    setInterval(() => {
+      handleOnNext();
+    }, 5000);
+  };
+  const handleOnNext = () => {
+    count = (count + 1) % sliderImages.length;
+    setImageIndex(count);
+    if (sliderRef.current) {
+      sliderRef.current.classList.add("fade-anim");
+    }
   };
 
   useEffect(() => {
-    slideRef.current.addEventListener("animationend", removeAnimation);
-    slideRef.current.addEventListener("mouseenter", pauseSlider);
-    slideRef.current.addEventListener("mouseleave", startSlider);
-
+    sliderRef.current.addEventListener("animationend", removeAnimation);
     startSlider();
-    return () => {
-      pauseSlider();
-    };
     // eslint-disable-next-line
   }, []);
 
-  const startSlider = () => {
-    slideInterval = setInterval(() => {
-      handleOnNextClick();
-    }, 5000);
+  const handleOnPrev = () => {
+    const productsLength = sliderImages.length;
+    count = (imageIndex + productsLength - 1) % productsLength;
+    setImageIndex(count);
+    sliderRef.current.classList.add("fade-anim");
   };
 
-  const pauseSlider = () => {
-    clearInterval(slideInterval);
-  };
-
-  const handleOnNextClick = () => {
-    count = (count + 1) % featuredProducts.length;
-    setCurrentIndex(count);
-    slideRef.current.classList.add("fade-anim");
-  };
-  const handleOnPrevClick = () => {
-    const productsLength = featuredProducts.length;
-    count = (currentIndex + productsLength - 1) % productsLength;
-    setCurrentIndex(count);
-    slideRef.current.classList.add("fade-anim");
+  const removeAnimation = () => {
+    sliderRef.current.classList.remove("fade-anim");
   };
 
   return (
-    <div ref={slideRef} className="w-full select-none relative pt-4">
-      <div className="aspect-w-16 aspect-h-9">
-        <img src={featuredProducts[currentIndex]} alt="" />
-      </div>
-
-      <div className="absolute w-full top-1/2 transform -translate-y-1/2 px-3 flex justify-between items-center">
+    <div ref={sliderRef} className="w-full select-none relative pt-10 flex">
+      <img src={sliderImages[imageIndex]} alt="" />
+      <div className="absolute w-full top-1/2 transform -translate-y-1/2 px-3 flex justify-between  text-2xl text-gray-500 ">
         <button
-          className="bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
-          onClick={handleOnPrevClick}
+          className="hover:text-gray-800  hover:scale-100"
+          onClick={handleOnPrev}
         >
-          <FaArrowCircleLeft size={30} />
+          <FaArrowCircleLeft />
         </button>
         <button
-          className="bg-black text-white p-1 rounded-full bg-opacity-50 cursor-pointer hover:bg-opacity-100 transition"
-          onClick={handleOnNextClick}
+          className="hover:text-gray-800  hover:scale-100"
+          onClick={handleOnNext}
         >
-          <BsArrowRightCircleFill size={30} />
+          <BsArrowRightCircleFill />
         </button>
       </div>
     </div>
   );
 };
+
 export default Hero;
