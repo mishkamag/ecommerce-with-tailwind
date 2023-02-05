8128e7f6fba906015722ee9bd1c79dea0d819fd6
mishkamag <m93.maglaperidze@gmail.com>
2023-02-06 00:15:44 +0400

    some css changes in invoice component

--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -7,38 +7,48 @@ const Invoice = () => {
   console.log(cart);
 
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md">
-      <table className="w-full text-left">
-        <thead>
-          <tr className="text-sm font-medium text-gray-600 uppercase ">
-            <th className="py-3 px-4">Title</th>
-            <th className="py-3 px-4">Image</th>
-            <th className="py-3 px-4">Amount</th>
-            <th className="py-3 px-4">Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.map((item, index) => (
-            <tr key={index} className="text-sm text-gray-700">
-              <td className="py-3 px-4">{item.title}</td>
-              <td className="py-3 px-4">
-                <img
-                  src={item.image}
-                  alt={item.title}
-                  className="w-16 h-16 object-cover rounded-full"
-                />
-              </td>
-              <td className="py-3 px-4">{item.amount}</td>
-              <td className="py-3 px-4">{`$ ${(
-                item.price * item.amount
-              ).toFixed(2)}`}</td>
+    <div className="container">
+      <header className=" bg-white px-4 py-6 border-b border-gray-200 sm:px-6">
+        <h1>Ecomerce</h1>
+        <h3>Kutaisi</h3>
+        <h3>Kutaisi</h3>
+        <h3>Kutaisi</h3>
+        <h3>Kutaisi</h3>
+      </header>
+
+      <div className="bg-white p-6 rounded-lg shadow-md ">
+        <table className="w-full text-left">
+          <thead>
+            <tr className="text-sm font-medium text-white uppercase border bg-gray-700 ">
+              <th className="py-3 px-4">Title</th>
+              <th className="py-3 px-4">Image</th>
+              <th className="py-3 px-4">Amount</th>
+              <th className="py-3 px-4">Price</th>
             </tr>
-          ))}
-          <h1 className="text-red-700 uppercase text-xl">
-            {totalPrice.toFixed(2)}
-          </h1>
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {cart.map((item, index) => (
+              <tr key={index} className="text-sm text-gray-700">
+                <td className="py-3 px-4">{item.title}</td>
+                <td className="py-3 px-4">
+                  <img
+                    src={item.image}
+                    alt={item.title}
+                    className="w-16 h-16 object-cover rounded-full"
+                  />
+                </td>
+                <td className="py-3 px-4">{item.amount}</td>
+                <td className="py-3 px-4">{`$ ${(
+                  item.price * item.amount
+                ).toFixed(2)}`}</td>
+              </tr>
+            ))}
+            <h1 className="text-red-700 uppercase text-xl">
+              {totalPrice.toFixed(2)}
+            </h1>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
