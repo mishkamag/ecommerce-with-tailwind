05e9a61e0da02d2035537ced68d4d5a7bdd0c16f
mishkamag <m93.maglaperidze@gmail.com>
2023-02-06 00:30:21 +0400

    some css in invoice component

--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -16,7 +16,7 @@ const Invoice = () => {
         <h3>Kutaisi</h3>
       </header>
 
-      <div className="bg-white p-6 rounded-lg shadow-md ">
+      <div className="bg-white p-6 rounded-lg shadow-md max-h-[450px] lg:max-h-[480px] overflow-y-auto overflow-x-hidden ">
         <table className="w-full text-left">
           <thead>
             <tr className="text-sm font-medium text-white uppercase border bg-gray-700 ">
@@ -29,16 +29,18 @@ const Invoice = () => {
           <tbody>
             {cart.map((item, index) => (
               <tr key={index} className="text-sm text-gray-700">
-                <td className="py-3 px-4">{item.title}</td>
-                <td className="py-3 px-4">
+                <td className="py-3 px-4 border font-semibold">{item.title}</td>
+                <td className="py-3 px-4 border">
                   <img
                     src={item.image}
                     alt={item.title}
                     className="w-16 h-16 object-cover rounded-full"
                   />
                 </td>
-                <td className="py-3 px-4">{item.amount}</td>
-                <td className="py-3 px-4">{`$ ${(
+                <td className="py-3 px-4 border font-semibold">
+                  {item.amount}
+                </td>
+                <td className="py-3 px-4 border font-bold">{`$ ${(
                   item.price * item.amount
                 ).toFixed(2)}`}</td>
               </tr>
