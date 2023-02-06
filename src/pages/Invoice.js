629a6717e0343264d6bbb749f62da674092dbde9
mishkamag <m93.maglaperidze@gmail.com>
2023-02-07 01:14:02 +0400

    some css changes in invoice

--- src/pages/Invoice.js
+++ src/pages/Invoice.js
@@ -1,23 +1,34 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext } from "react";
 import { CartContext } from "../store/CartContext";
 
 const Invoice = () => {
   const { cart, totalPrice } = useContext(CartContext);
 
   return (
-    <div className="container">
-      <header className=" bg-white px-4 py-6 border-b border-gray-200 sm:px-6">
-        <h1>Ecomerce</h1>
-        <h3>Kutaisi</h3>
-        <h3>Kutaisi</h3>
-        <h3>Kutaisi</h3>
-        <h3>Kutaisi</h3>
+    <div className="">
+      <header className="  bg-gray-100 px-4 py-6 border-b border-gray-200 sm:px-6 flex justify-between">
+        <div>
+          <div className="mb-4 w-[90px]">
+            <img
+              src="https://www.pngitem.com/pimgs/m/161-1619330_logos-e-commerce-png-transparent-png.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="mb-4">
+          <div className="font-bold mb-2">From:</div>
+          <div className="text-gray-600">
+            Ecommerce <br />
+            Buxaidze str 19/77 <br />
+            Kutaisi 4600
+          </div>
+        </div>
       </header>
 
-      <div className="bg-white p-6 rounded-lg shadow-md max-h-[450px] lg:max-h-[480px] overflow-y-auto overflow-x-hidden ">
+      <div className="bg-white  rounded-lg shadow-md max-h-[300px] lg:max-h-[330px] overflow-y-auto overflow-x-hidden  ">
         <table className="w-full text-left">
           <thead>
-            <tr className="text-sm font-medium text-white uppercase border bg-gray-700 ">
+            <tr className="text-sm font-medium text-white uppercase border bg-[#008ECC]  ">
               <th className="py-3 px-4">Title</th>
               <th className="py-3 px-4">Image</th>
               <th className="py-3 px-4">Amount</th>
@@ -43,19 +54,23 @@ const Invoice = () => {
                 ).toFixed(2)}`}</td>
               </tr>
             ))}
-            {/* <p className="text-red-700 uppercase text-xl">
-              {totalPrice.toFixed(2)}
-            </p> */}
           </tbody>
         </table>
-        <div className="mt-4">
+      </div>
+      <div className="flex w-full ">
+        <div className="  bg-[#008ECC] text-white font-bold py-2 px-12 rounded   mt-4">
+          TOTAL: {totalPrice.toFixed(2)}
+        </div>
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
           <input
             type="email"
             placeholder="Email"
-            className="appearance-none border rounded p-2 w-64 mr-2"
+            className="appearance-none border rounded p-2 w-55 mr-2"
           />
 
-          <button className="bg-indigo-500 hover:bg-indigo-700 text-white font-medium py-2 px-4 rounded ml-2">
+          <button className="bg-[#008ECC] hover:bg-[#008EAC] text-white font-medium py-2 px-4 rounded ml-2">
             Send
           </button>
         </div>
