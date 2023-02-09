f1114a2f68eef587194c7f771a6f5023fdff83d5
mishkamag <m93.maglaperidze@gmail.com>
2023-02-09 21:02:42 +0400

    integrate email js ,have key bug

--- src/pages/Invoice.js
+++ src/pages/Invoice.js
@@ -2,10 +2,11 @@ import React, { useContext, useRef, useState } from "react";
 import { CartContext } from "../store/CartContext";
 import { jsPDF } from "jspdf";
 import autoTable from "jspdf-autotable";
+import emailjs from "emailjs-com";
 
 const Invoice = () => {
   const { cart, totalPrice } = useContext(CartContext);
-  // const [emailInput, setEmailInput] = useState("");
+  const [email, setEmail] = useState("");
 
   const [pdf, setPdf] = useState(null);
   const componentRef = useRef();
@@ -23,41 +24,29 @@ const Invoice = () => {
     });
     setPdf(doc.output("datauristring"));
   };
-  console.log(pdf);
-  // const base64 = btoa(pdf);
-  // console.log(base64);
 
-  // const data = {
-  //   to: emailInput,
-  //   from: "mishka.maglaperidze@yahoo.com",
-  //   subject: "Invoice",
-  //   text: "Invoice",
-  //   attachments: [
-  //     {
-  //       content: base64,
-  //       filename: "invoice.pdf",
-  //       type: "application/pdf",
-  //       disposition: "attachment",
-  //     },
-  //   ],
-  // };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    handleGenerate();
+    const result = await emailjs.send(
+      "gBc71-RpPzO0bJXH4",
+      "service_vw8fu2d",
+      "template_tfe9hys",
 
-  // const options = {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
-  //   },
-  //   body: JSON.stringify(data),
-  // };
+      {
+        to_email: email,
+        attachments: [
+          {
+            type: "application/pdf",
+            name: "products.pdf",
+            content: pdf,
+          },
+        ],
+      }
+    );
 
-  // fetch("http://localhost:3000", options)
-  //   .then((response) => {
-  //     console.log(response);
-  //   })
-  //   .catch((error) => {
-  //     console.error(error);
-  //   });
+    console.log(result);
+  };
 
   return (
     <div className="">
@@ -121,25 +110,62 @@ const Invoice = () => {
         </div>
       </div>
       <div className="mt-4 flex justify-between">
-        <div>
-          <input
-            // onChange={(e) => setEmailInput(e.target.value)}
-            type="email"
-            placeholder="Email"
-            className="appearance-none border rounded p-2 w-55 mr-2"
-          />
-
-          <button
-            onClick={handleGenerate}
-            className="bg-[#008ECC] hover:bg-[#008EAC] text-white font-medium py-2 px-4 rounded ml-2"
-          >
-            Send
-          </button>
-          {/* {pdf && <iframe title="generated pdf" src={pdf} />} */}
-        </div>
+        <form onSubmit={handleSubmit} className="w-full max-w-sm mx-auto">
+          <div className="flex items-center border-b border-teal-500 py-2">
+            <input
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            />
+            <button
+              type="submit"
+              className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+            >
+              Send
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
 };
 
 export default Invoice;
+
+// {pdf && <iframe title="generated pdf" src={pdf} />}
+// const base64 = btoa(pdf);
+// console.log(base64);
+
+// const data = {
+//   to: emailInput,
+//   from: "mishka.maglaperidze@yahoo.com",
+//   subject: "Invoice",
+//   text: "Invoice",
+//   attachments: [
+//     {
+//       content: base64,
+//       filename: "invoice.pdf",
+//       type: "application/pdf",
+//       disposition: "attachment",
+//     },
+//   ],
+// };
+
+// const options = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+//   },
+//   body: JSON.stringify(data),
+// };
+
+// fetch("http://localhost:3000", options)
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
