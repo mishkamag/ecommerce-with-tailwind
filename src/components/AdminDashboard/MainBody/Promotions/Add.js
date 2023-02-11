e84c2e9fedab61fdb86cf8dffa79641f2473b8cc
Slomtadze <slomtadze90@gmail.com>
2023-02-11 11:48:42 +0400

    Working on Add Promotion MarkUp

--- src/components/AdminDashboard/MainBody/Promotions/Add.js
+++ src/components/AdminDashboard/MainBody/Promotions/Add.js
@@ -2,10 +2,42 @@ import React from "react";
 
 const Add = () => {
   return (
-    <form>
-      <label htmlFor="promotion">Add Promotion</label>
-      <input type="file" name="promotion" id="promotion" />
-    </form>
+    <div className="absolute h-full w-full bg-black/70 rounded-xl flex items-center justify-center">
+      <div className="w-3/4 h-3/4 bg-white rounded-xl">
+        <div className="flex justify-center items-center w-full h-[10%] text-xl bg-slate-50 rounded-t-xl">
+          <span>New Promotion</span>
+        </div>
+        <form className="h-[90%] py-4 px-8 rounded-b-xl">
+          <div className="h-[20%] flex justify-between items-center">
+            <div className="flex flex-col ">
+              <label htmlFor="title">Add Promotion Title</label>
+              <input
+                className="italic font-mono outline-none py-2"
+                type="text"
+                name="title"
+                id="title"
+                placeholder="Enter Promotion Title"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="promotion">Add Promotion Image</label>
+              <input
+                className="italic font-mono outline-none py-2"
+                type="file"
+                name="promotion"
+                id="promotion"
+              />
+            </div>
+            <button className="px-4 py-2 bg-green-200 rounded-2xl hover:bg-green-300">
+              + Add
+            </button>
+          </div>
+          <div className="w-full h-[80%] border-2 border-dashed flex justify-center items-center ">
+            <span className="border-dashed border-2 p-4">No Image Choosed</span>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
--- src/components/AdminDashboard/MainBody/Promotions/Promotions.js
+++ src/components/AdminDashboard/MainBody/Promotions/Promotions.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import Add from "./Add";
 import List from "./List";
 
 const Promotions = () => {
+  const [addItem, setAddItem] = useState(true);
   return (
-    <div className="h-full w-full bg-gray-100 rounded-xl ">
+    <div className="relative h-full w-full bg-gray-100 rounded-xl ">
+      {addItem ? <Add /> : null}
       <div className="w-full h-[10%] flex justify-between items-center  rounded-t-xl px-4">
         <h1 className="text-2xl ">Active Promotions</h1>
+        <button>Add Promotion</button>
+        <button>Ended Promotions</button>
       </div>
       <List />
     </div>
