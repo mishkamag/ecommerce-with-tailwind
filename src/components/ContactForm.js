0800f82540e153b28ee16b748e67cff6446b05ef
mishkamag <m93.maglaperidze@gmail.com>
2023-02-27 00:10:56 +0400

    change form like a modalform

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -35,108 +35,128 @@ function ContactForm({ cart }) {
   };
 
   return (
-    <form
-      ref={form}
-      onSubmit={sendEmail}
-      className="lg:max-w-[500px] lg:mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 "
-    >
-      <div className="w-full h-full flex justify-center items-center">
-        {" "}
-        {isLoading && <Spinner />}
-      </div>
-      <div>
-        {isSent && (
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center">
-            <div className="flex justify-center items-center bg-white rounded-md px-4 py-2">
-              <p className="text-lg font-bold mb-2">
-                თქვენი შეკვეთა გაგზავნილია
-                <br />
-                რამოდენიმე წუთში თქვენ დაგიკვაშირდებათ ჩვენი წარმომადგენელი{" "}
-                <br />
-                მადლობთ რომ ხართ ჩვენი მომხმარებელი
-              </p>
-              <Link to="/">
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                  onClick={() => setIsSent(false)}
+    <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0 ">
+        <div className="fixed inset-0 transition-opacity">
+          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              className="lg:max-w-[500px] lg:mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 "
+            >
+              <div className="w-full h-full flex justify-center items-center">
+                {" "}
+                {isLoading && <Spinner />}
+              </div>
+              <div>
+                {isSent && (
+                  <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center">
+                    <div className="flex justify-center items-center bg-white rounded-md px-4 py-2">
+                      <p className="text-lg font-bold mb-2">
+                        თქვენი შეკვეთა გაგზავნილია
+                        <br />
+                        რამოდენიმე წუთში თქვენ დაგიკვაშირდებათ ჩვენი
+                        წარმომადგენელი <br />
+                        მადლობთ რომ ხართ ჩვენი მომხმარებელი
+                      </p>
+                      <Link to="/">
+                        <button
+                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                          onClick={() => setIsSent(false)}
+                        >
+                          დახურვა
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                )}
+              </div>
+
+              <div className="mb-4 ">
+                <label
+                  className="block text-gray-700 font-bold mb-2"
+                  htmlFor="name"
                 >
-                  დახურვა
+                  Name
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="name"
+                  name="user_name"
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Enter your name"
+                />
+              </div>
+
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 font-bold mb-2"
+                  htmlFor="phone"
+                >
+                  Phone Number
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="phone"
+                  type="tel"
+                  name="user_tel"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  placeholder="Enter your phone number"
+                />
+              </div>
+
+              <div className="mb-6">
+                <label
+                  className="block text-gray-700 font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email Address
+                </label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  name="user_email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email address"
+                />
+              </div>
+              <div>
+                <textarea
+                  name="pdf"
+                  className="hidden"
+                  rows={cart.length + 1}
+                  value={cart
+                    .map(
+                      (product) =>
+                        `პროდუქტის დასახელება :${product.title} რაოდენობა: ${product.amount} ფასი: ${product.price} `
+                    )
+
+                    .join("\n")}
+                  readOnly
+                />
+              </div>
+
+              <div className="flex items-center justify-center">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Submit
                 </button>
-              </Link>
-            </div>
+              </div>
+            </form>
           </div>
-        )}
+        </div>
       </div>
-
-      <div className="mb-4 ">
-        <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
-          Name
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="name"
-          name="user_name"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Enter your name"
-        />
-      </div>
-
-      <div className="mb-4">
-        <label className="block text-gray-700 font-bold mb-2" htmlFor="phone">
-          Phone Number
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="phone"
-          type="tel"
-          name="user_tel"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-          placeholder="Enter your phone number"
-        />
-      </div>
-
-      <div className="mb-6">
-        <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
-          Email Address
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="email"
-          name="user_email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your email address"
-        />
-      </div>
-      <div>
-        <textarea
-          name="pdf"
-          className="hidden"
-          rows={cart.length + 1}
-          value={cart
-            .map(
-              (product) =>
-                `პროდუქტის დასახელება :${product.title} რაოდენობა: ${product.amount} ფასი: ${product.price} `
-            )
-
-            .join("\n")}
-          readOnly
-        />
-      </div>
-
-      <div className="flex items-center justify-center">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
+    </div>
   );
 }
 
