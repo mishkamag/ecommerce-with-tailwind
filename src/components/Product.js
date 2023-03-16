dcd80e0a7b1c0a765e72af8b132289396aa1c126
mishkamag <m93.maglaperidze@gmail.com>
2023-03-16 23:48:05 +0400

    fixed bug

--- src/components/Product.js
+++ src/components/Product.js
@@ -41,9 +41,9 @@ const Product = ({ product }) => {
       </div>
 
       <div className="text-sm capitalize text-gray-500 mb-1">{category}</div>
-      <Link to={`product/${id}`}>
-        <h2 className="font-semibold mb-1  ">{title}</h2>
-      </Link>
+
+      <h2 className="font-semibold mb-1  ">{title}</h2>
+
       <div className="font-semibold">$ {price}</div>
     </div>
   );
